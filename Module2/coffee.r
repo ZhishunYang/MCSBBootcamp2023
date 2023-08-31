7ff2eb19461f38ca6af20ce175dac7547acda633
Zhishun_Yang <91224175+ZhishunYang@users.noreply.github.com>
2023-08-30 18:19:51 -0700

    Update coffee.r
    updated after module 2

--- Module2/coffee.r
+++ Module2/coffee.r
@@ -3,18 +3,40 @@
 # n - number of days
 # x - fraction of caffeinated
 
-nMax <- 20 # max number of days to simulate
+nMax <- 1000 # max number of days to simulate
 
-N <- 10 # number of scoops in each jar
+r <- 2.9
+K <- 0.6
 x <- rep(0, nMax) # fraction caffeinated
-x[1] <- 1.0 # initial fraction caffeinated
+x[1] <- 0.2 # initial fraction caffeinated
 
 for (n in 2:nMax) {
-  x[n] <- (1 - 1/N) * x[n-1]
+  x[n] <- x[n-1] + r*(1-x[n-1]/K)*x[n-1]
 }
 
-# THE MODEL ^
-# ------------------------------------------
-# THE BEHAVIOR / THE OUTPUT ? 
+plot(x, type='o', pch=16, col='black', ylab='fraction caffeinated', xlab='Days',xlim = c(nMax-6,nMax))
+x
 
-plot(x, type='o', pch=16, col='black', ylab='fraction caffeinated', xlab='Days')
+#-------g
+nMax=1000
+r=seq(0,2.9,0.1)
+x=matrix(NA,nMax,length(r))
+x[1,]=rep(0.2,length(r))
+K=0.6
+
+for(ri in 1:length(r)){
+  for (n in 2:nMax) {
+    x[n,ri] <- x[n-1,ri] + r[ri]*(1-x[n-1,ri]/K)*x[n-1,ri]
+  }
+}
+
+#get the last 100 obs 
+xmat=x[901:1000,]
+
+#plot
+plot(NULL,xlab="r",ylab="pop",xlim=c(0,3),ylim=c(0,1))
+for (i in 1:length(r)) {
+  points(x=rep(r[i],dim(xmat)[1]),y=xmat[,i])
+}
+
+
